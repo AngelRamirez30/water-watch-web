7674206ef64ad2b65ad9c31939d3e0aad3e9c372
Angel Ramirez <angelgabrielramirez30@gmail.com>
2024-08-18 16:29:16 -0700

    ApiTesting

--- src/app/services/base-api.service.ts
+++ src/app/services/base-api.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../enviroments/enviroment.dev';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseApiService {
 
+  private apiUrl = environment.apiUrl;
+
   constructor() {}
 
-  public baseUrl = '/api';
+  public baseUrl = this.apiUrl;
 }
