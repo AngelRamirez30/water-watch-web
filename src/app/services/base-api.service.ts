a2fb212543aaea8502800eabf8c1963dc6103c19
Angel Ramirez <angelgabrielramirez30@gmail.com>
2024-08-18 17:01:39 -0700

    Dislexia error...

--- src/app/services/base-api.service.ts
+++ src/app/services/base-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../enviroments/enviroment';
+import { environment } from '../../enviroments/environment';
 
 @Injectable({
   providedIn: 'root'
